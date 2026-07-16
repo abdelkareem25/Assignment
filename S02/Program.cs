51187c34ede3fd651dbbdcee6674a665b44b7d81
Abdelkariem Hany <74042738+abdelkareem25@users.noreply.github.com>
2026-07-16 15:12:45 +0300

    Q6

--- S02/Program.cs
+++ S02/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             #region Q1
+            Console.WriteLine("Q1:");
             /*
              Create a Book class with a Title (string) and Pages (int).
              Create a Book object and store it in a variable of type object. Print it.
@@ -16,9 +17,9 @@
             };
             Console.WriteLine(b);
             #endregion
-
-
             #region Q2
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Q2:");
             /*
              * Using the Book class above, print the result of calling ToString(),
              * Equals() (compare book with itself), GetHashCode(), and GetType() on book
@@ -28,12 +29,12 @@
             Console.WriteLine(b.GetHashCode());
             Console.WriteLine(b.GetType());
             #endregion
-
             #region Q3
-
+            //Cannot understand the question
             #endregion
-
             #region Q4
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Q4:");
             /*
              Write code that divides 10 by 0 inside a try block,
              catches the exception, prints "Cannot divide by zero", and then prints "Done" in a finally block.
@@ -52,6 +53,26 @@
             {
                 Console.WriteLine("Done");
             }
+            #endregion
+            #region Q5
+            /*
+             Declare an int pages = 300; then store it in a double variable without using a cast.
+             */
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Q5:");
+            int pages = 300;
+            double pagesDouble = pages;
+            Console.WriteLine(pagesDouble);
+            #endregion
+            #region Q6
+            /*
+             Declare a double price = 49.99; then convert it into an int using a cast.
+             */
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Q6:");
+            double price = 49.99;
+            int priceInt = (int)price;
+            Console.WriteLine(priceInt);
             #endregion
         }
     }
